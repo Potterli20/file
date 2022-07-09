1bfcea2dcde785d5616493554993230747f77f22
trli <potterli520@gmail.com>
2022-07-10 02:04:51 +0800

    Update aria2.sh

--- aria2/aria2.sh
+++ aria2/aria2.sh
@@ -563,7 +563,7 @@ crontab_update_start() {
     crontab -l >"/tmp/crontab.bak"
     sed -i "/aria2.sh update-bt-tracker/d" "/tmp/crontab.bak"
     sed -i "/tracker.sh/d" "/tmp/crontab.bak"
-    echo -e "\n0 7 * * * /bin/bash <(wget -qO- https://raw.githubusercontent.com/Potterli20/file/main/aria2/tracker.sh) ${aria2_conf} RPC 2>&1 | tee ${aria2_conf_dir}/tracker.log" >>"/tmp/crontab.bak"
+    echo -e "\n0 7 * * * /bin/bash <(wget -qO- https:/file-git.trli.club/aria2/tracker.sh) ${aria2_conf} RPC 2>&1 | tee ${aria2_conf_dir}/tracker.log" >>"/tmp/crontab.bak"
     crontab "/tmp/crontab.bak"
     rm -f "/tmp/crontab.bak"
     if [[ -z $(crontab_update_status) ]]; then
