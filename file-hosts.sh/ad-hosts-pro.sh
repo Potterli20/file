1572b9d171642e6024791466e2b444627a70d8dc
trli <potterli520@gmail.com>
2022-12-25 19:01:43 +0800

    Update ad-hosts-pro.sh

--- file-hosts.sh/ad-hosts-pro.sh
+++ file-hosts.sh/ad-hosts-pro.sh
@@ -357,6 +357,7 @@ function GetData() {
         "https://raw.githubusercontent.com/stamparm/blackbook/master/blackbook.txt"
     )
     filter_hosts=(
+        "https://raw.githubusercontent.com/lingeringsound/10007_auto/master/all"
         "https://raw.githubusercontent.com/yous/YousList/master/hosts.txt"
         "https://raw.githubusercontent.com/smed79/blacklist/master/hosts.txt"
         "https://sysctl.org/cameleon/hosts"
