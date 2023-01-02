c93a62e1b972cbe62d172928ff0c8d88e7b2cf96
trli <potterli520@gmail.com>
2023-01-02 17:48:05 +0800

    Update allow.sh

--- file-hosts.sh/allow.sh
+++ file-hosts.sh/allow.sh
@@ -360,12 +360,11 @@ cat ./filter_adblock.tmp | grep '^[@@]' | sed '/^$/d' | sed 's/[ ]*//g' | sort -
 cat ./filter_domain.tmp | grep -v "#\|!" | sed '/^$/d' | sed 's/[ ]*//g' | sed "/./{s/^/@@||&/;s/$/&^/}" | sort -u > allow-domain.txt
 cat allow-domain.txt allow-adblock.txt | sed '/^$/d' | sed 's/[ ]*//g' | sort -u > allow.txt
 
-num=`cat allow.txt | wc -l`
-echo "! Title: trli's Filter for Allowlist" >> allow.txt
-echo "! Description: HOSTS Project" >> allow.txt
-echo "! Expires: 24 hours (update frequency)" >> allow.txt
-echo "! Homepage: https://file.trli.club:2083/allow/" >> allow.txt
-echo "! Version: `TZ=UTC-8 date +"%Y-%m-%d %H:%M:%S"`" >> allow.txt
-echo "! Total count: $num" >> allow.txt
+sed -i '1cTitle: trli's Filter for Allowlist' allow.txt
+sed -i '2cDescription: HOSTS Project' allow.txt
+sed -i '3cExpires: 24 hours (update frequency)' allow.txt
+sed -i '4cHomepage: https://file.trli.club:2083/allow/' allow.txt
+sed -i '5cVersion: `$(TZ=UTC-8 date +"%Y-%m-%d %H:%M:%S")`' allow.txt
+sed -i '6cTotal count: '$(wc -l allow.txt)'' allow.txt
 
 exit
