4b5476ae7d063d9185aa5c274d3fcae7ffc705d4
Your Name <potterli520@gmail.com>
2023-01-17 10:42:38 +0800

    add dns

--- file/dns/dns.sh
+++ file/dns/dns.sh
@@ -1,4 +1,5 @@
-sudo su
-systemctl disable --now systemd-resolved.service
-rm /etc/resolv.conf
-echo 'nameserver 1.1.1.1' > /etc/resolv.conf
+sudo sed -i 's/#DNS=/DNS=8.8.8.8 8.8.4.4/g' /etc/systemd/resolved.conf
+sudo systemctl daemon-reload
+sudo systemctl restart systemd-networkd
+sudo systemctl restart systemd-resolved
+sudo prlimit --pid $$ --nofile=500000:500000
