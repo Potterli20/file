ae0b42286b0189fc7b5136f639b066831d076fc0
trli <potterli520@gmail.com>
2023-01-18 14:53:49 +0800

    Update dns.sh

--- file/dns/dns.sh
+++ file/dns/dns.sh
@@ -1,7 +1,8 @@
-sudo sed -i 's/#DNS=/DNS=1.1.1.1 1.0.0.1/g' /etc/systemd/resolved.conf
+sudo sed -i 's/#DNS=/DNS=1.1.1.1 1.0.0.1 223.5.5.5 223.6.6.6/g' /etc/systemd/resolved.conf
 sudo systemctl daemon-reload
 sudo systemctl restart systemd-networkd
 sudo systemctl restart systemd-resolved
 sudo prlimit --pid $$ --nofile=500000:500000
 ulimit -a
+sudo apt-get -qq update
 sudo apt reinstall curl
