04f1e24747e8be5ff2ee24b05c63d7f60df57f88
trli <potterli520@gmail.com>
2023-01-18 16:24:15 +0800

    Update dns.sh

--- file/dns/dns.sh
+++ file/dns/dns.sh
@@ -1,4 +1,4 @@
-sudo sed -i 's/#DNS=/DNS=1.1.1.1 1.0.0.1 223.5.5.5 223.6.6.6/g' /etc/systemd/resolved.conf
+sudo sed -i 's/#DNS=/DNS=223.5.5.5 223.6.6.6/g' /etc/systemd/resolved.conf
 sudo systemctl daemon-reload
 sudo systemctl restart systemd-networkd
 sudo systemctl restart systemd-resolved
