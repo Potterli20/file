129bbde79feb2856d2fa0f8465c57c678b5a872b
trli <potterli520@gmail.com>
2023-01-22 22:16:30 +0800

    Update ad-hosts-sqlist.sh

--- file-hosts.sh/ad-hosts-sqlist.sh
+++ file-hosts.sh/ad-hosts-sqlist.sh
@@ -322,6 +322,7 @@ function GetData() {
         "https://filters.adtidy.org/extension/ublock/filters/224_optimized.txt"
     )
     filter_domain=(
+        "https://raw.githubusercontent.com/groovy-sky/SaferDNS/main/blocklists/domains/Ultimate.Hosts.Blacklist.txt"
         "https://raw.githubusercontent.com/slyfox1186/pihole-regex/main/domains/blacklist/exact-blacklist.txt"
         "https://raw.githubusercontent.com/neodevpro/neodevhost/master/domain"
         "https://raw.githubusercontent.com/notracking/hosts-blocklists-scripts/master/domains.dead.txt"
