8f7a4bd2418d14bdca277757725d3e140435c639
trli <potterli520@gmail.com>
2023-02-21 14:55:29 +0800

    Update hosts.sh

--- file-hosts.sh/github/hosts.sh
+++ file-hosts.sh/github/hosts.sh
@@ -1,2 +1,1 @@
 python ./file-hosts.sh/github/ph.py -f ./Domains/AccelerateHosts/Domains -o ./file-hosts.sh/github/Accelerate-Hosts.txt
-python ./file-hosts.sh/github/ph.py -f ./Domains/cn-blocked-domain/domains.txt -o ./file-hosts.sh/github/gfw-Hosts.txt
