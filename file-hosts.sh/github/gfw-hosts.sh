e9aede25f49ebdd16ded1b9641aed833216eb194
unknown <yc_ligy@sdyc.com>
2023-04-18 18:03:25 +0800

    update gfw-hosts

--- file-hosts.sh/github/gfw-hosts.sh
+++ file-hosts.sh/github/gfw-hosts.sh
@@ -1,1 +1,2 @@
-python ./file-hosts.sh/github/ph.py -f ./Domains/cn-blocked-domain/domains.txt -o ./file-hosts.sh/github/gfw-Hosts.txt
+wget -O ./file-hosts.sh/github/domains.txt https://github.com/Potterli20/file/releases/download/cn-blocked-domain/domains.txt
+python ./file-hosts.sh/github/ph.py -f ./file-hosts.sh/github/domains.txt -o ./file-hosts.sh/github/gfw-Hosts.txt
