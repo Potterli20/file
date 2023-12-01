201540ea2228b86c1e542cb35aaeea1b3a3e09d8
trli <potterli520@gmail.com>
2023-12-01 14:40:35 +0800

    Update dns-adgh-lite-https-all.sh

--- file-hosts.sh/dns-hosts/dns-adgh-lite-https-all.sh
+++ file-hosts.sh/dns-hosts/dns-adgh-lite-https-all.sh
@@ -199,6 +199,7 @@ function GenerateRules() {
             "h3://dns.alidns.com/dns-query"
         )
         foreign_dns=(
+            "https://dns64.cloudflare-dns.com/dns-query"
             "https://77.88.8.8:443/dns-query"
             "https://firefox.dns.nextdns.io/dns-query"
             "https://doh.opendns.com/dns-query"
